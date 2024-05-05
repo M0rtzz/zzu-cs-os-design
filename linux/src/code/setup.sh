1fd7a9fcfde85d44533e6f46a920ef350d9e8184
M0rtzz <M0rtzz@noreply.gitcode.com>
2024-05-05 14:06:44 +0800

    🔨 script(setup.sh): update logic

--- linux/src/code/setup.sh
+++ linux/src/code/setup.sh
@@ -1,27 +1,27 @@
-#!/usr/bin/sudo /bin/bash
+#!/usr/bin/sudo /bin/zsh
 
-# 如果在编译 bochs 时遇到 libgtk2.0-dev 依赖问题，请手动执行
+# 如果在编译 bochs 时遇到 libgtk2.0-dev 依赖问题，请手动执行：
 # sudo apt update && sudo apt install aptitude && sudo aptitude install libgtk2.0-dev
 
-_echo_info(){
+_echo_info() {
     echo -e "[Info]: ${1}"
 }
 
-_echo_err(){
+_echo_err() {
     echo -e "\033[31m[Error]: ${1} \033[0m"
 }
 
-_echo_succ(){
+_echo_succ() {
     echo -e "\033[32m[Succ]: ${1} \033[0m"
 }
 
-env_install(){
+env_install() {
 
-    sudo apt-get install -y make bin86 gcc-multilib &> /dev/null \
-    && _echo_succ "bin86 is installed." || _echo_err "bin86 is not installed!!!"
+    sudo apt install -y make bin86 gcc-multilib &>/dev/null &&
+        _echo_succ "bin86 is installed." || _echo_err "bin86 is not installed!!!"
 
-    if [ ! -z `which gcc-3.4` ];then
-        _echo_succ "Gcc-3.4 is installed."
+    if [ ! -z $(which gcc-3.4) ]; then
+        _echo_succ "GCC-3.4 is installed."
         return
     fi
 
@@ -35,34 +35,37 @@ env_install(){
         "libstdc++6-dev_3.4.6-6ubuntu3_amd64.deb"
     )
 
-    if [ -z `which gcc-3.4` ]; then
+    if [ -z $(which gcc-3.4) ]; then
         _echo_info "Start installing gcc-3.4..."
         for deb in ${DOWNLOAD_LIST[*]}; do
             if [ ! -e ${GCC_DIR}/${deb} ]; then
                 # wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/${deb} -P ${GCC_DIR} -q --show-progress && \
-                wget http://mirrors.ustc.edu.cn/ubuntu-old-releases/ubuntu/pool/universe/g/gcc-3.4/${deb} -P ${GCC_DIR} -q --show-progress && \
-                _echo_info "Download ${deb} Sucessfully." || ( rm ${deb} & _echo_err "Download ${deb} unsuccessfully!!!" )
+                # NOTE: 替换为中科大镜像站
+                wget http://mirrors.ustc.edu.cn/ubuntu-old-releases/ubuntu/pool/universe/g/gcc-3.4/${deb} -P ${GCC_DIR} -q --show-progress &&
+                    _echo_info "Download ${deb} Sucessfully." || (
+                    rm ${deb} &
+                    _echo_err "Download ${deb} unsuccessfully!!!"
+                )
             fi
         done
-        sudo dpkg -i ${GCC_DIR}/*.deb &> /dev/null
-        sudo apt-get install -y -f &> /dev/null
-        if [ ! -z `which gcc-3.4` ];then
+        sudo dpkg -i ${GCC_DIR}/*.deb &>/dev/null
+        sudo apt install -y -f &>/dev/null
+        if [ ! -z $(which gcc-3.4) ]; then
             _echo_succ "gcc-3.4 is installed."
         fi
         rm -rf ${GCC_DIR}
     fi
 }
 
-bochs_install(){
+bochs_install() {
 
-    sudo apt-get install -y build-essential libgtk2.0-dev \
+    sudo apt install -y build-essential libgtk2.0-dev \
         libx11-dev xserver-xorg-dev xorg-dev g++ \
         pkg-config libxcursor-dev libxrandr-dev \
-        libxinerama-dev libxi-dev &> /dev/null
+        libxinerama-dev libxi-dev &>/dev/null
 
-    sudo apt-get install -y build-essential &> /dev/null
-    sudo apt-get install -y bochs bochs-x bochs-sdl &> /dev/null
-    
+    sudo apt install -y build-essential &>/dev/null
+
     # INFO:https://github.com/oldlinux-web/oldlinux-files/blob/master/bochs/README_FIRST
     # NOTE: M0rtzz have resolved the bug in the source code
     # if [ ! -e "bochs-2.2.6.tar.gz" ]; then
@@ -73,41 +76,53 @@ bochs_install(){
 
     # if [ ! -d "bochs-2.2.6" ];then
     #     tar zxvf bochs-2.2.6.tar.gz &> /dev/null && \
-    #     _echo_info "tar bochs-2.2.6.tar.gz Sucessfullyhttp://mirrors.ustc.edu.cn/ubuntu-old-releases/." || \
+    #     _echo_info "tar bochs-2.2.6.tar.gz Sucessfully." || \
     #     (rm -rf ../bochs-2.2.6 & _echo_err "tar bochs-2.2.6.tar.gz unsuccessfully!!!" )
     # fi
 
-    if [ -d "bochs-2.2.6" ];then
+    if [ -d "/usr/local/share/bochs/" ]; then
+        sudo rm -rf "/usr/local/share/bochs/"
+    fi
+
+    if [ -d "bochs-2.2.6" ]; then
         cd bochs-2.2.6
         make clean
-        if [ "$1" ] && [ "$1" = "-d" ];then
-        # sudo apt-get install aptitude && sudo apt install libgtk2.0-dev
-        sudo apt update && sudo apt install libgtk2.0-dev
-        # ./configure --enable-gdb-stub --enable-disasm 
-        ./configure --enable-gdb-stub --enable-new-pit --enable-all-optimizations --enable-4meg-pages --enable-global-pages --enable-pae --enable-sep --enable-cpu-level=6 --enable-sse=2 --disable-reset-on-triple-fault --with-all-libs
-        # ./configure --enable-debugger --enable-disasm
-        # ./configure --enable-disasm --enable-debugger --enable-new-pit --enable-all-optimizations --enable-4meg-pages --enable-global-pages --enable-pae --enable-sep --enable-cpu-level=6 --enable-sse=2 --disable-reset-on-triple-fault --with-all-libs
-        make -j$(nproc) && (cp bochs ./bochsdbg & cp bochs ../bochs-gdb & cp bochs ../../../oslab/bochs/bochs-gdb & _echo_succ "make bochs sucessfully.") && sudo make install || _echo_err "make bochs unsucessfully.!!!"
+        if [ "$1" ] && [ "$1" = "-d" ]; then
+            # sudo apt install aptitude && sudo apt install libgtk2.0-dev
+            sudo apt update && sudo apt install libgtk2.0-dev
+            # ./configure --enable-gdb-stub --enable-disasm
+            ./configure --enable-gdb-stub --enable-new-pit --enable-all-optimizations --enable-4meg-pages --enable-global-pages --enable-pae --enable-sep --enable-cpu-level=6 --enable-sse=2 --disable-reset-on-triple-fault --with-all-libs
+            # ./configure --enable-debugger --enable-disasm
+            # ./configure --enable-disasm --enable-debugger --enable-new-pit --enable-all-optimizations --enable-4meg-pages --enable-global-pages --enable-pae --enable-sep --enable-cpu-level=6 --enable-sse=2 --disable-reset-on-triple-fault --with-all-libs
+            make -j$(nproc) && (
+                cp bochs ./bochsdbg &
+                cp bochs ../bochs-gdb &
+                cp bochs ../../../oslab/bochs/bochs-gdb &
+                _echo_succ "make bochs sucessfully."
+            ) && sudo make install || _echo_err "make bochs unsucessfully.!!!"
         else
-        ./configure --enable-gdb-stub --enable-new-pit --enable-all-optimizations --enable-4meg-pages --enable-global-pages --enable-pae --enable-sep --enable-cpu-level=6 --enable-sse=2 --disable-reset-on-triple-fault --with-all-libs
-        make -j$(nproc) && (cp bochs ./bochsdbg & cp bochs ../bochs-gdb & cp bochs ../../../oslab/bochs/bochs-gdb & _echo_succ "make bochs sucessfully.") && sudo make install || _echo_err "make bochs unsucessfully.!!!"
+            ./configure --enable-gdb-stub --enable-new-pit --enable-all-optimizations --enable-4meg-pages --enable-global-pages --enable-pae --enable-sep --enable-cpu-level=6 --enable-sse=2 --disable-reset-on-triple-fault --with-all-libs
+            make -j$(nproc) && (
+                cp bochs ./bochsdbg &
+                cp bochs ../bochs-gdb &
+                cp bochs ../../../oslab/bochs/bochs-gdb &
+                _echo_succ "make bochs sucessfully."
+            ) && sudo make install || _echo_err "make bochs unsucessfully.!!!"
         fi
     fi
 
 }
 
 trap 'onCtrlC' INT
-function onCtrlC () {
+function onCtrlC() {
     _echo_err "[Warning]: stopped by user."
     rm -rf ${GCC_DIR}
     exit
 }
 echo " 须知"
-# echo "     请根据oslab/README.txt中的第2点，修改下载的bochs-2.2.6源码，并重新执行该脚本"
 echo "脚本将完成以下两件事："
 echo "     1. 为系统安装相应的编译环境（make，bin86，gcc-3.4，gcc-multilib）"
 echo "     2. 在脚本当前目录及\${OSLAB_PATH}/bochs/下生成一个bochs-gdb（若没有生成,使用./setup.sh -d 重新执行脚本）并执行sudo make install将bochs可执行文件安装进/usr/local/bin/目录下等安装操作（doc，share，man等等）"
 
 env_install
 bochs_install $1
-
