272999684273d50ba53e78a6d4fcc54ec434969f
M0rtzz <M0rtzz@noreply.gitcode.com>
2024-05-11 14:53:09 +0800

    ♻️ refactor(run.sh): shellcheck

--- linux/tools/run.sh
+++ linux/tools/run.sh
@@ -6,7 +6,8 @@ if [ -f "${lock_file}" ]; then
     rm "${lock_file}"
 fi
 
-export TOOLS_PATH=$(dirname `which $0`)
+TOOLS_PATH=$(dirname "$(command -v "$0")")
+export TOOLS_PATH
 
 if [ ! -e "hdc.img" ]; then
 tar -xvJf hdc.tar.xz
@@ -14,11 +15,11 @@ fi
 
 if [ "$1" ] && [ "$1" = "-m" ]
 then
-(cd ../linux-0.12; make clean; make; cp Image ../tools/Image)
+(cd ../linux-0.12 || exit; make clean; make; cp Image ../tools/Image)
 elif [ "$1" ] && [ "$1" = "-g" ]
 then
-${TOOLS_PATH}/bochs/bochs-gdb -q -f ${TOOLS_PATH}/bochs/bochsrc-gdb.bxrc & \
-gdb -x ${TOOLS_PATH}/bochs/.gdbrc ../linux-0.12/tools/system
+"${TOOLS_PATH}"/bochs/bochs-gdb -q -f "${TOOLS_PATH}"/bochs/bochsrc-gdb.bxrc & \
+gdb -x "${TOOLS_PATH}"/bochs/.gdbrc ../linux-0.12/tools/system
 else
-bochs -q -f ${TOOLS_PATH}/bochs/bochsrc.bxrc
+bochs -q -f "${TOOLS_PATH}"/bochs/bochsrc.bxrc
 fi
